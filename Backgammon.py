0c854556dff40570fa66028668d27e9bfb975446
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2025-01-28 00:06:57 +0330

    feat: implement get_possible_sequences method for generating moves
    - Added `get_possible_sequences` to generate possible move sequences based on the dice roll.
    - Introduced `generate_moves` to handle generating individual moves for each die.

--- Backgammon.py
+++ Backgammon.py
@@ -73,3 +73,28 @@ class MiniMaxBackgammon(Backgammon):
             if self.board[i][1] == self.current_player:
                 return False
         return True
+    
+    def get_possible_sequences(self, dice_roll: List[int]) -> List[List[Tuple[int, int]]]:
+        '''
+        The idea is to generate all possible moves for each die in the dice roll and then combine them to form sequences.
+        First we generate all possible moves for the first die, then for the second die.
+        For each die, we start from the points where the current player has pieces and move them forward or backward based on the die value. Then we check if the end point is valid; If so we add the move to the list of possible moves.
+        We then combine the moves for the two dice to form sequences.
+        If the dice values are different, we also generate sequences for the reverse order of the dice values.
+        '''
+
+        def generate_moves(dice: List[int], player: str) -> List[List[Tuple[int, int]]]:
+            '''
+            Generate all possible moves for the given dice values. It does this by iterating over all points where the player has pieces and then moving them forward or backward based on the die value. Then add to the list of possible moves if the end point is valid.
+            '''
+            moves = []
+            for die in dice:
+                for i in range(24):
+                    if self.board[i][1] == player:
+                        end_pos = i + die if player == "white" else i - die
+                        if 0 <= end_pos < 24 and (self.board[end_pos][1] in [None, player] or self.board[end_pos][0] == 1):
+                            moves.append([(i, end_pos)])
+            return moves
+        
+        first_moves = generate_moves([dice_roll[0]], self.current_player)
+        second_moves = generate_moves([dice_roll[1]], self.current_player)
